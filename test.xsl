ec073e872f9d097bf7f09a4a7ac4ac65b32f781f
Johnson Lau <gatement@gmail.com>
2013-09-22 08:48:25 +0800

    init

---
+++ test.xsl
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+	<xsl:template match="/order">
+		order - <xsl:apply-templates select="item"/> 
+	</xsl:template>
+	<xsl:template match="item">
+		name: <xsl:value-of select="name" />,
+		quantity: <xsl:value-of select="quantity" />;
+	</xsl:template>
+</xsl:stylesheet>
---
+++ transformer.py
@@ -0,0 +1,49 @@
+import libxml2, libxslt  
+
+class compoundXML:  
+	def __init__(self):  
+		self._result = None  
+		self._xsl = None  
+		self._xml = None  
+
+
+	def do(self, xml_file_name, xsl_file_name):         
+		self._xml = libxml2.parseFile(xml_file_name)  
+		if self._xml == None:  
+			return 0  
+		styledoc = libxml2.parseFile(xsl_file_name)  
+		if styledoc == None:  
+			return 0  
+		self._xsl = libxslt.parseStylesheetDoc(styledoc)  
+		if self._xsl == None:  
+			return 0  
+
+		self._result = self._xsl.applyStylesheet(self._xml, None)                 
+
+	def get_xml_doc(self):  
+		return self._result             
+
+	def get_translated(self):  
+		return self._result.serialize('UTF-8')         
+
+	def save_translated(self, file_name):  
+		self._xsl.saveResultToFilename(file_name, self._result, 0)  
+
+	def release(self):  
+		''''' 
+		this function must be called in the end. 
+		'''  
+		self._xsl.freeStylesheet()  
+		self._xml.freeDoc()  
+		self._result.freeDoc()  
+		self._xsl = None  
+		self._xml = None  
+		self._result = None  
+
+if __name__ == '__main__':  
+	test = compoundXML()  
+	test.do('test.xml', 'test.xsl')  
+	print test.get_translated()  
+	test.save_translated('test.txt')  
+	test.release() 
+
